f740dcca99c1bae628ab93dcc04b0a31eb8b58b9
NakshatraGandhi <nakshath2003@gmail.com>
2026-04-07 12:37:13 +0530

    updated DB config for Aiven

--- config/db.js
+++ config/db.js
@@ -1,15 +1,22 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "bookzen",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.connect((err) => {
-  if (err) console.log(err);
-  else console.log("DB Connected");
+  if (err) {
+    console.error("DB connection failed:", err);
+  } else {
+    console.log("DB Connected ✅");
+  }
 });
 
 module.exports = db;
